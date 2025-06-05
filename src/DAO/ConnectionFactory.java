1e8493662991e265a600fa0d509c1b562c88c34c
Vinicius colazelli <vinicolazelli@yahoo.com.br>
2025-06-05 10:26:45 -0300

    Criando conexão com MySql

---
+++ src/DAO/ConnectionFactory.java
@@ -0,0 +1,23 @@
+package DAO;
+
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+
+public class ConnectionFactory 
+{
+    public Connection getConnection(){
+        Connection conn = null;
+        try 
+        {
+            String url = "jdbc:mysql://localhost:3306/java?user=root&password=password";
+            conn = DriverManager.getConnection(url);
+        } catch (SQLException erro) 
+        {
+            JOptionPane.showMessageDialog(null, erro.getMessage());
+        }
+        return conn;
+    }
+}
