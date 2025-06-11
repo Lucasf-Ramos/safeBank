5b5a49da26c71f619b92afe3bac0280516346666
Vinicius colazelli <vinicolazelli@yahoo.com.br>
2025-06-11 09:20:35 -0300

    add: adicionei o model de Transação.

---
+++ src/model/Transacao.java
@@ -0,0 +1,55 @@
+package model;
+
+import java.util.Date;
+
+public class Transacao {
+    private long id;
+    private Conta contaOrigem;
+    private Conta contaDestino;
+    private double valor;
+    private Date data_transferencia;
+
+    public Transacao(long id, Conta contaOrigem, Conta contaDestino, double valor, Date data_transferencia) {
+        this.id = id;
+        this.contaOrigem = contaOrigem;
+        this.contaDestino = contaDestino;
+        this.valor = valor;
+        this.data_transferencia = data_transferencia;
+    }
+
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Conta getContaOrigem() {
+        return contaOrigem;
+    }
+    public void setContaOrigem(Conta contaOrigem) {
+        this.contaOrigem = contaOrigem;
+    }
+
+    public Conta getContaDestino() {
+        return contaDestino;
+    }
+    public void setContaDestino(Conta contaDestino) {
+        this.contaDestino = contaDestino;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public Date getData_transferencia() {
+        return data_transferencia;
+    }
+
+    public void setData_transferencia(Date data_transferencia) {
+        this.data_transferencia = data_transferencia;
+    }
+}
